37b0ec03f2ff8796c546f3118943a9199304e59d
OmkarSavalkar <65709013+OmkarSavalkar@users.noreply.github.com>
2023-02-14 21:37:32 +0530

    winning logic improved

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -13,23 +13,34 @@ const Board = () => {
   const clickArea = (id) => {
     if (winner === "") {
       document.getElementById(id).innerHTML = value ? "X" : "O";
+      document.getElementById(id).disabled = true;
       let x = xArray;
       let o = oArray;
       if (value) {
         x.push(id);
+        document.getElementById(id).disabled = true;
         setXArray(x);
       } else {
         o.push(id);
+        document.getElementById(id).disabled = true;
         setOArray(o);
       }
       if (x.length > 2 || o.length > 2) {
         winning.forEach((item) => {
-          if (x.join("").includes(item)) {
+          if (
+            document.getElementById(item[0]).innerHTML === "X" &&
+            document.getElementById(item[1]).innerHTML === "X" &&
+            document.getElementById(item[2]).innerHTML === "X"
+          ) {
             document.getElementById(item[0]).style.backgroundColor = "yellow";
             document.getElementById(item[1]).style.backgroundColor = "yellow";
             document.getElementById(item[2]).style.backgroundColor = "yellow";
             setWinner("X");
-          } else if (o.join("").includes(item)) {
+          } else if (
+            document.getElementById(item[0]).innerHTML === "O" &&
+            document.getElementById(item[1]).innerHTML === "O" &&
+            document.getElementById(item[2]).innerHTML === "O"
+          ) {
             document.getElementById(item[0]).style.backgroundColor = "yellow";
             document.getElementById(item[1]).style.backgroundColor = "yellow";
             document.getElementById(item[2]).style.backgroundColor = "yellow";
@@ -37,7 +48,7 @@ const Board = () => {
           }
         });
       }
-      if (x.length + o.length >= 8) {
+      if (x.length + o.length === 9 && winner === "") {
         setDraw(true);
       }
     }
@@ -87,24 +98,39 @@ const Board = () => {
             className={style["input"]}
             id={1}
             onClick={(e) => {
-              setValue(!value);
-              clickArea(e.target.id);
+              if (
+                document.getElementById(e.target.id).innerHTML !== "X" &&
+                document.getElementById(e.target.id).innerHTML !== "O"
+              ) {
+                setValue(!value);
+                clickArea(e.target.id);
+              }
             }}
           ></div>
           <div
             className={style["input"]}
             id={2}
             onClick={(e) => {
-              setValue(!value);
-              clickArea(e.target.id);
+              if (
+                document.getElementById(e.target.id).innerHTML !== "X" &&
+                document.getElementById(e.target.id).innerHTML !== "O"
+              ) {
+                setValue(!value);
+                clickArea(e.target.id);
+              }
             }}
           ></div>
           <div
             className={style["input"]}
             id={3}
             onClick={(e) => {
-              setValue(!value);
-              clickArea(e.target.id);
+              if (
+                document.getElementById(e.target.id).innerHTML !== "X" &&
+                document.getElementById(e.target.id).innerHTML !== "O"
+              ) {
+                setValue(!value);
+                clickArea(e.target.id);
+              }
             }}
           ></div>
         </div>
@@ -113,24 +139,39 @@ const Board = () => {
             className={style["input"]}
             id={4}
             onClick={(e) => {
-              setValue(!value);
-              clickArea(e.target.id);
+              if (
+                document.getElementById(e.target.id).innerHTML !== "X" &&
+                document.getElementById(e.target.id).innerHTML !== "O"
+              ) {
+                setValue(!value);
+                clickArea(e.target.id);
+              }
             }}
           ></div>
           <div
             className={style["input"]}
             id={5}
             onClick={(e) => {
-              setValue(!value);
-              clickArea(e.target.id);
+              if (
+                document.getElementById(e.target.id).innerHTML !== "X" &&
+                document.getElementById(e.target.id).innerHTML !== "O"
+              ) {
+                setValue(!value);
+                clickArea(e.target.id);
+              }
             }}
           ></div>
           <div
             className={style["input"]}
             id={6}
             onClick={(e) => {
-              setValue(!value);
-              clickArea(e.target.id);
+              if (
+                document.getElementById(e.target.id).innerHTML !== "X" &&
+                document.getElementById(e.target.id).innerHTML !== "O"
+              ) {
+                setValue(!value);
+                clickArea(e.target.id);
+              }
             }}
           ></div>
         </div>
@@ -139,28 +180,47 @@ const Board = () => {
             className={style["input"]}
             id={7}
             onClick={(e) => {
-              setValue(!value);
-              clickArea(e.target.id);
+              if (
+                document.getElementById(e.target.id).innerHTML !== "X" &&
+                document.getElementById(e.target.id).innerHTML !== "O"
+              ) {
+                setValue(!value);
+                clickArea(e.target.id);
+              }
             }}
           ></div>
           <div
             className={style["input"]}
             id={8}
             onClick={(e) => {
-              setValue(!value);
-              clickArea(e.target.id);
+              if (
+                document.getElementById(e.target.id).innerHTML !== "X" &&
+                document.getElementById(e.target.id).innerHTML !== "O"
+              ) {
+                setValue(!value);
+                clickArea(e.target.id);
+              }
             }}
           ></div>
           <div
             className={style["input"]}
             id={9}
             onClick={(e) => {
-              setValue(!value);
-              clickArea(e.target.id);
+              if (
+                document.getElementById(e.target.id).innerHTML !== "X" &&
+                document.getElementById(e.target.id).innerHTML !== "O"
+              ) {
+                setValue(!value);
+                clickArea(e.target.id);
+              }
             }}
           ></div>
         </div>
-        {winner !== "" ? (
+        <div className={style["input-row"]}>
+          <div className={style["madeby"]}> ©️ Made By Omkar Savalkar</div>
+        </div>
+
+        {winner !== "" && !draw ? (
           <>
             <h2 className={style["w-color"]}>
               <span className={style["player-win"]}>{winner}</span> Wins the
@@ -177,7 +237,7 @@ const Board = () => {
         ) : (
           <></>
         )}
-        {draw ? (
+        {draw && winner === "" ? (
           <>
             <h2 style={{ color: "orange" }}>
               <span style={{ color: "red" }}>Draw !</span> Both Played Well !
@@ -208,7 +268,35 @@ const Board = () => {
         />
       </div>
       <div id={"info"} className={style["instruction-div"]}>
-        njjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+        <div style={{ margin: "5% 15px 5px 15px" }}>
+          You probably already know how to play Tic-Tac-Toe. It's a really
+          simple game, right? That's what most people think.
+          <br />
+          <br />
+          RULES FOR TIC-TAC-TOE <br />
+          <br />
+          1. The game is played on a grid that's 3 squares by 3 squares. <br />
+          2. You are X, your friend (or the computer in this case) is O. Players
+          take turns putting their marks in empty squares.
+          <br /> 3. The first player to get 3 of her marks in a row (up, down,
+          across, or diagonally) is the winner.
+          <br /> 4. When all 9 squares are full, the game is over. <br />
+          If no player has 3 marks in a row, the game ends in a tie. <br />
+          <br />
+          HOW CAN I WIN AT TIC-TAC-TOE? <br />
+          <br /> Part of your strategy is trying to figure out how to get three
+          Xs in a row. The other part is trying to figure out how to stop the
+          other player from getting three Os in a row. After you put an X in a
+          square, you start looking ahead. Where's the best place for your next
+          X? You look at the empty squares and decide which ones are good
+          choices—which ones might let you make three Xs in a row. You also have
+          to watch where the other player puts its O. That could change what you
+          do next. If the player gets two Os in a row, you have to put your next
+          X in the last empty square in that row, or the other player will win.
+          If you always pay attention and look ahead, you'll never lose a game
+          of Tic-Tac-Toe. You may not win, but at least you'll tie.
+        </div>
+        <div style={{ color: "yellow" }}> ©️ Made By Omkar Savalkar</div>
       </div>
     </div>
   );
